bddbdc0a9af9e2ced90ead91eb1047afcb87f7bf
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-13 11:26:21 -0400

    Adicionando styled inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ export default {
     './src/**/*.{ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'primary': "var(--primary)",
+      'primary-dark':"var(--primary-dark)",
+      'primary-light':"var(--primary-light)",
+      'secondary': "var(--secondary)",
+      'background': "var(--background)",
+      'task-bg': "var(--task-bg)",
+      'tag-bg': "var(--tag-bg)",
+      'tag-color': "var(--tag-color)",
+      'alert': 'var(--alert)'
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      inter: ['Inter', 'serif'],
+    },
+    },
   },
   plugins: [],
 }
